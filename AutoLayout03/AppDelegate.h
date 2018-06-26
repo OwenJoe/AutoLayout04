1604b202e2a9ecbd947d7dd84a7b44c82a5a10c3
owen <zhuwb@hori-gz.com>
2018-06-26 12:01:37 +0800

    测试demo

---
+++ AutoLayout03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout03
+//
+//  Created by owen on 2018/6/26.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout03
+//
+//  Created by owen on 2018/6/26.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
